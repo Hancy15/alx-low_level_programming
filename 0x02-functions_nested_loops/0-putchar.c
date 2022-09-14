548b24383d8b6eaa80f1f4493232b220cc606bd7
Hancy15 <sadiqmaliyu01@gmail.com>
2022-09-14 11:16:01 -0700

    alpha

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
